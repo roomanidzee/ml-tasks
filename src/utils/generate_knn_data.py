a8413bbb2fc480cdce2a35152f48bf7e734c4b5f
Андрей Романов <romanov_andrey_vl@protonmail.com>
2019-11-06 10:58:21 +0300

    generated knn data

---
+++ src/utils/generate_knn_data.py
@@ -0,0 +1,13 @@
+import csv
+import random
+
+labels = ['A', 'B', 'C', 'D', 'E', 'F']
+
+with open('knn_data.csv', 'w') as csv_file:
+
+    data_writer = csv.writer(csv_file, delimiter=',')
+
+    for _ in range(100_000):
+
+        centerX, centerY = random.random() * 5.0, random.random() * 5.0
+        data_writer.writerow([random.gauss(centerX, 0.5), random.gauss(centerY, 0.5), random.choice(labels)])
