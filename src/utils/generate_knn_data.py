83053a7199b7cd2ee0ad9b1df16ae068b59aa97a
Андрей Романов <romanov_andrey_vl@protonmail.com>
2019-11-06 15:28:02 +0300

    fixed classificate data

--- src/utils/generate_knn_data.py
+++ src/utils/generate_knn_data.py
@@ -7,6 +7,8 @@ with open('knn_data.csv', 'w') as csv_file:
 
     data_writer = csv.writer(csv_file, delimiter=',')
 
+    data_writer.writerow(['x', 'y', 'label'])
+
     for _ in range(100_000):
 
         centerX, centerY = random.random() * 5.0, random.random() * 5.0
