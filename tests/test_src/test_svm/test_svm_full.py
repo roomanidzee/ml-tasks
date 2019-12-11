069283b3339703e0efbd09e967b7265569bdbd57
Андрей Романов <romanov_andrey_vl@protonmail.com>
2019-12-11 23:27:05 +0300

    svm notebook

--- tests/test_src/test_svm/test_svm_full.py
+++ tests/test_src/test_svm/test_svm_full.py
@@ -7,24 +7,26 @@ from src.svm.full import classify
 
 def test_classify():
 
-    n = 100
+    n = 150
 
-    class1 = npr.rand(n // 2, 2)
-    class2 = npr.rand(n // 2, 2) + np.array([1, 0])
+    class1 = npr.rand(n // 3, 2)
+    class2 = npr.rand(n // 3, 2)
+    class3 = npr.rand(n // 3, 2)
 
     theta = np.pi / 8.0
     r = np.cos(theta)
     s = np.sin(theta)
     rotation = np.array([[r, s], [s, -r]])
 
-    samples = np.dot(np.vstack([class1, class2]), rotation)
+    samples = np.dot(np.vstack([class1, class2, class3]), rotation)
 
     labels = np.zeros(n)
 
-    half = n // 2
+    third = n // 3
 
-    labels[:half] = -1
-    labels[half:] = 1
+    labels[:third] = 0
+    labels[third:third - 20] = 1
+    labels[third:] = 2
 
     p, q, g, h, x = classify(samples, labels)
 
